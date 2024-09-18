fc3004f57cab0be07dc362b58a697e6b199d3c32
Liviu Chis <chis@adobe.com>
2024-09-17 17:01:04 -0700

    Added to feed component

--- blocks/feed/feed.js
+++ blocks/feed/feed.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-underscore-dangle */
 export default async function decorate(block) {
   const props = [...block.children];
+  const roleFolderPairs = {};
+  const firstRole = props[0].textContent.trim();
+  const firstFolder = props[0].textContent.trim();
+  roleFolderPairs[firstRole] = firstFolder; 
   
-  console.log(props);
+  console.log(roleFolderPairs);
 }
