8cb69ba334f616d425ef11e79b0f8057abb529c4
Liviu Chis <chis@adobe.com>
2024-09-17 17:02:14 -0700

    Added to feed component

--- blocks/feed/feed.js
+++ blocks/feed/feed.js
@@ -3,7 +3,7 @@ export default async function decorate(block) {
   const props = [...block.children];
   const roleFolderPairs = {};
   const firstRole = props[0].textContent.trim();
-  const firstFolder = props[0].textContent.trim();
+  const firstFolder = props[1].textContent.trim();
   roleFolderPairs[firstRole] = firstFolder; 
   
   console.log(roleFolderPairs);
