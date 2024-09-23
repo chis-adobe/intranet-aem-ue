7c0af7b2261577add011c463981edd147350de44
Liviu Chis <chis@adobe.com>
2024-09-22 17:33:31 -0700

    Adjusted header and feed components

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -114,7 +114,8 @@ export function decorateNavAuth() {
 export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/en/nav';
+  const urlBranch = window.location.pathname.split('/')[0];
+  const navPath = navMeta ? new URL(navMeta).pathname : `${urlBranch}/nav`;
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
